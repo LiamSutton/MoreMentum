b3040dc9812356a906b9f23ee027770502c4d80e
Liam Sutton <liamsutton1997@outlook.com>
2020-10-16 16:14:49 +0100

    Basic VoiceLineManager script implemented to prevent overlapping voice lines

---
+++ Assets/Scripts/VoiceLineManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceLineManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public VoiceLineTrigger[] children;
+    void Start()
+    {
+        children = GetComponentsInChildren<VoiceLineTrigger>();
+    }
+
+    public void StopCurrentlyPlayingVoiceLines() 
+    {
+        foreach (VoiceLineTrigger trig in children)
+        {
+            if (trig.isCurrenltyPlaying) {
+                Debug.Log(trig.voiceLine.name + " IS CURRENTLY PLAYING  AT: " + Time.time);
+                trig.StopVoiceLine();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/VoiceLineTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceLineTrigger : MonoBehaviour
+{
+    public AudioClip voiceLine;
+    public bool isCurrenltyPlaying; 
+
+    // When the player enters the trigger, play the voice line associated with it.
+    public void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("VoiceLineTriggers").GetComponent<VoiceLineManager>().StopCurrentlyPlayingVoiceLines();
+        Debug.Log("STARTING PLAYBACK FOR: " + voiceLine.name + " AT: " + Time.time);
+        StartCoroutine("PlayVoiceLine");
+    }
+
+    // Dissable the trigger when the player exits so that the voice line wont repeat or be played again if the player walks back through it
+    public void OnTriggerExit(Collider other)
+    {
+        GetComponent<Collider>().enabled = false;
+    }
+
+    public IEnumerator PlayVoiceLine() {
+        isCurrenltyPlaying = true;
+        GetComponent<AudioSource>().PlayOneShot(voiceLine);
+        yield return new WaitWhile(() => GetComponent<AudioSource>().isPlaying);
+        isCurrenltyPlaying = false;
+    }
+
+    public void StopVoiceLine() {
+        Debug.Log("STOPPING PLAYBACK FOR " + voiceLine.name + " AT: " + Time.time);
+        GetComponent<AudioSource>().Stop();
+    }
+}
