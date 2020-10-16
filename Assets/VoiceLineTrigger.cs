0a99bc46a9455fbc3c334bcd674f2c362236596c
Liam Sutton <liamsutton1997@outlook.com>
2020-10-16 15:47:18 +0100

    Added basic voice line triggers

---
+++ Assets/VoiceLineTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceLineTrigger : MonoBehaviour
+{
+    public AudioClip voiceLine;
+
+    // When the player enters the trigger, play the voice line associated with it.
+    public void OnTriggerEnter(Collider other)
+    {
+        GetComponent<AudioSource>().PlayOneShot(voiceLine);
+    }
+
+    // Dissable the trigger when the player exits so that the voice line wont repeat or be played again if the player walks back through it
+    public void OnTriggerExit(Collider other)
+    {
+        GetComponent<Collider>().enabled = false;
+    }
+}
