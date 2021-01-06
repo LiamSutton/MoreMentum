34ff40f9ded8a3e9f76179f5d17ac7e494e3c9a4
Liam Sutton <liamsutton1997@outlook.com>
2021-01-06 15:48:21 +0000

    Began implementing pause menu

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject gameOverlayUI;
+    public GameObject player;
+
+
+
+    void Start() {
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (gameIsPaused) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    void Pause() {
+        Cursor.visible = true;
+        player.GetComponent<PlayerController>().enabled = false;
+        pauseMenuUI.SetActive(true);
+        gameOverlayUI.SetActive(false);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    void Resume() {
+        Cursor.visible = false;
+        player.GetComponent<PlayerController>().enabled = true;
+        pauseMenuUI.SetActive(false);
+        gameOverlayUI.SetActive(true);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+}
