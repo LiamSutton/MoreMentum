c389ba14943f38c9a3f650fa194a25eb236022fc
Liam Sutton <liamsutton1997@outlook.com>
2021-01-06 13:52:21 +0000

    Fly menu coming along nicely

---
+++ Assets/Scripts/CameraMountController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMountController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform currentMount;
+    public float stepSpeed = 0.1f;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, currentMount.position, stepSpeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, currentMount.rotation, stepSpeed);
+    }
+
+    public void SetMount(Transform newMount) {
+        currentMount = newMount;
+    }
+}
