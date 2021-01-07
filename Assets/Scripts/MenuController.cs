f83a94f0e1c847b9f1aff52a424d108c695037cf
Liam Sutton <liamsutton1997@outlook.com>
2021-01-07 10:36:03 +0000

    added button click, began adding more menus

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
-    public void StartGame() {
+    AudioSource audioSource;
+    AudioClip buttonClickSound;
+
+    public void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        buttonClickSound = audioSource.clip;
+    }
+    public void StartGame()
+    {
         SceneManager.LoadScene(1);
     }
 
-    public void LoadLevel(int sceneIndex) {
+    public void LoadLevel(int sceneIndex)
+    {
         SceneManager.LoadScene(sceneIndex);
     }
 
-    public void ExitGame() {
+    public void ExitGame()
+    {
         Application.Quit();
+    }
+
+    public void PlayButtonClickSound()
+    {
+        audioSource.PlayOneShot(buttonClickSound);
     }
 }
