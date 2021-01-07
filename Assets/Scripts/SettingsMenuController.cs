f987e9382f3b030725a6ded18395455ff4dc3f9b
Liam Sutton <liamsutton1997@outlook.com>
2021-01-07 11:41:05 +0000

    Master audio can now be controlled

---
+++ Assets/Scripts/SettingsMenuController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenuController : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetVolume(float newVolume)
+    {
+        mixer.SetFloat("MasterVolume", newVolume);
+    }
+}
